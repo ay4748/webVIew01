68541a66a982f27116716a7ee3196c00f8bc854d
ay4748 <ay4748@bs.amalnet.k12.il>
2024-02-18 12:08:56 +0200

    final

--- app/src/main/java/com/example/webview01/MainActivity.java
+++ app/src/main/java/com/example/webview01/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.webview01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText eT;
+    WebView wV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eT = findViewById(R.id.eT);
+        wV = findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.setWebViewClient(new MyWebViewClient());
     }
+
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public void dodge1(View view) {
+        String url = eT.getText().toString();
+        wV.loadUrl(url);
+    }
+
 }
